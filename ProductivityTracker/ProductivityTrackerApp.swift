742ae685fb372d567c3bcb0581201abadc6dbb88
Rishabh Tewatia <rishabh1teotia@gmail.com>
2026-03-19 13:21:19 +0530

    Initial Commit

---
+++ ProductivityTracker/ProductivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityTrackerApp.swift
+//  ProductivityTracker
+//
+//  Created by Rishabh on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
